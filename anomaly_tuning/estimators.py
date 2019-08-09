771041bce2f69c4aa9c4d144290780b1b522a774
Albert Thomas <albertthomas88@gmail.com>
2019-08-09 23:18:38 +0200

    add sigma in OCSVM init

--- anomaly_tuning/estimators.py
+++ anomaly_tuning/estimators.py
@@ -228,6 +228,7 @@ class OCSVM(OneClassSVM):
     def __init__(self, sigma=1.0, nu=0.4):
         gamma = 1. / (2. * sigma ** 2)
         super(OCSVM, self).__init__(gamma=gamma, nu=nu)
+        self.sigma = sigma
 
 
 class IsolationForest(ensemble.IsolationForest):
